e78d074640a5b2aec2f980df94e8bfbaf5c6c311
Aniket Chakraborty <aniketc713@gmail.com>
2021-12-06 20:31:35 +0530

    Initial commit

---
+++ main.py
@@ -0,0 +1,51 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.")
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = input("Do you want to take a left of right?").lower()
+if direction == "left":
+  swimwait = input(
+      "You reached a river bank. Do you wanna wait or swim?").lower()
+  if swimwait == "wait":
+    door = input(
+        "Look its a house with three doors which are colored differently. Choose a door. Red, Yellow or Blue").lower()
+    if door == "red":
+      print("Oh no! There was fire behind the red door. Game Over.")
+    elif door == "yellow":
+      print("You found the treasure. You Win!")
+    elif door == "blue":
+      print("You were eaten by a beast behind the blue door. Game Over")
+    else:
+      print("Game Over.")
+  elif swimwait == "swim":
+    print("You were attacked a group of trouts. Game Over.")
+  else:
+    print("Game Over")
+elif direction == "right":
+  print("You fell into a hole. Game Over")
+else:
+  print("Game Over")
